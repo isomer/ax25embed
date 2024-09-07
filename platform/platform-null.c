f522996a7ab9d3c69c5f453993f9b5f4ace0d79f
Perry Lorier <git@isomer.meta.net.nz>
2024-09-07 12:47:14 +0100

    Improve some of the platform documentation.

--- platform/platform-null.c
+++ platform/platform-null.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* This returns a monotonic increasing timestamp. */
 instant_t instant_now(void) {
     struct timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
@@ -22,19 +23,29 @@ instant_t instant_now(void) {
                 duration_nanos(ts.tv_nsec)));
 }
 
+/* This should panic the system, potentially displaying a message */
 void panic(const char *msg) {
     DEBUG(STR(msg));
     abort();
 }
 
 void register_ticker(ticker_t *ticker) {
-    /* Don't ever run any tickers */
+    /* For the null platform we don't register (nor run) tickers, but for a
+     * functional system tickers registered with this should all be called
+     * relatively frequently (eg every 10ms).  Tickers can give a hint when
+     * they next should be called to allow lower power states.
+     */
 }
 
 void platform_init(int argc, char *argv[]) {
-    /* Nothing to do for POSIX */
+    /* Nothing to do by default */
 }
 
 void platform_run(void) {
-    /* Don't do anytihng */
+    /* This function should call ticker's occasionally, and call
+     * `void * serial_recv_byte(uint8_t serialport, uint8_t byte);` when a byte
+     * is received on a serialport.  serialport numbers are 0..15.
+     *
+     * This function is expected to not exit.
+     */
 }
--- platform/public/serial.h
+++ platform/public/serial.h
@@ -9,6 +9,14 @@
 
 void serial_putch(uint8_t serial, uint8_t data);
 void register_serial(uint8_t device, void (*rx)(uint8_t device, uint8_t ch), bool debug);
+
+/** Receive byte from device.
+ *
+ * Not reentrant. (Don't call from interrupt handlers).
+ *
+ * Device can be 0..15.
+ * Byte is the byte that was received on the serial device.
+ */
 void serial_recv_byte(uint8_t device, uint8_t byte);
 
 #endif
